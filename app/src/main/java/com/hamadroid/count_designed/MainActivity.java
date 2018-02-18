e3d3b023dc15196290d05deeb48f48d7378b0718
daich <daichithirteen@gmail.com>
2018-02-18 20:03:45 +0900

    Initial commit

---
+++ app/src/main/java/com/hamadroid/count_designed/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hamadroid.count_designed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        count=0;
+    }
+    public void plus(View view){
+    count =  count+1;
+    textView.setText(count+"");
+    }
+    public void minus (View view){
+    count = count-1;
+    textView.setText(count+"");
+    }
+}
